aa09aa7c30c49317df8ebd53e5b5f68d5025054a
kcheung3102 <kcheung3102@gmail.com>
2021-11-13 22:15:33 -0500

    made crypto currency state global with context api

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, 
+{ 
+    createContext, 
+    useContext, 
+    useState, 
+    useEffect } from 'react'
 
-export const CryptoContext = () => {
+export const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+    const [currency, setCurrency] = useState("USD")
+    const [symbol, setSymbol] = useState("$")
+
+
+    useEffect(() => {
+        switch(currency) {
+            case "USD":
+                setSymbol("$"); 
+                break;
+            case "JPY": 
+                setSymbol("¥")
+                break;
+            default:
+                    setSymbol("$")
+        }
+    }, [currency])
     return (
-        <div>
-            
-        </div>
+       <Crypto.Provider value={{ currency, setCurrency, symbol }}>
+            {children}
+       </Crypto.Provider>
     )
+}
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+    return (
+        useContext(Crypto)
+        );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React,{useState} from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from '@mui/material/Container';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 import './Navbar.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CryptoState } from "../CryptoContext";
 
 export const Navbar = () => {
+
+const { currency, setCurrency } = CryptoState();
+console.log(currency);
+
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+        mode: "dark",
+    },
+  });
+
   return (
-    <div>
+    <ThemeProvider theme={darkTheme}>
       <AppBar color="transparent" position="static">
       <Container>
         <Toolbar>
@@ -22,10 +43,20 @@ export const Navbar = () => {
           >
             CryptoWatcher
           </Typography>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={currency}
+          label="currency"
+          onChange={handleChange}
+        >
+          <MenuItem value={"USD"}>USD</MenuItem>
+          <MenuItem value={"JPY"}>JPY</MenuItem>
+        </Select>
           <Button color="inherit"> Login </Button>
         </Toolbar>
         </Container>
       </AppBar>
-    </div>
+    </ThemeProvider>
   );
 };
