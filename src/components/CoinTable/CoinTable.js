3bb07e396df0e8ee2525e8d5b3ddcd74242af55b
kcheung3102 <kcheung3102@gmail.com>
2021-12-04 09:48:35 -0500

    added sparkline chart to coin table

--- src/components/CoinTable/CoinTable.js
+++ src/components/CoinTable/CoinTable.js
@@ -6,37 +6,67 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CoinList } from "../../config/api";
 import { HistoricalChart } from "../../config/api";
 import { CryptoState } from "../../CryptoContext";
-import Pagination from '@mui/material/Pagination';
-import Grid from '@mui/material/Grid';
-import { Line } from 'react-chartjs-2';
+import Pagination from "@mui/material/Pagination";
+import Grid from "@mui/material/Grid";
+import { Line } from "react-chartjs-2";
+import { Link } from "react-router-dom";
+import { Container } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import moment from "moment";
 
 export const CoinTable = () => {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
-  const [days, setDays] = useState(7);
   const { currency, symbol } = CryptoState();
 
+  const formatSparkLine = (numbers) => {
+    const sevenDaysAgo = moment().subtract(7, 'days').unix();
+    let formattedSparkline = numbers.map((item, index) => {
+      return {
+        x: sevenDaysAgo + (index + 1) * 3600,
+        y: item
+      }
+    });
+    return formattedSparkline;
+  }
+
+  const formatMarketData = (data) => {
+    let formattedData = [];
+    data.forEach(item => {
+      const formattedSparkline = formatSparkLine(item.sparkline_in_7d.price);
+
+      const formattedItem = {
+        ...item,
+        sparkline_in_7d: {
+          price: formattedSparkline
+        }
+      }
+
+      formattedData.push(formattedItem);
+    });
+
+    return formattedData;
+  }
 
   const handleSearch = () => {
     //filters out the coins state on key press or on search click
     return coins.filter((coin) => {
       coin.name.toLowerCase().includes(search) ||
-      coin.symbol.toLowerCase().includes(search)
-    }
-    );
-  }
+        coin.symbol.toLowerCase().includes(search);
+    });
+  };
 
   const darkTheme = createTheme({
     palette: {
       primary: {
         main: "#fff",
       },
-        mode: "dark",
+      mode: "dark",
     },
   });
 
@@ -49,103 +79,115 @@ export const CoinTable = () => {
     }
 
     const data = await response.json();
-    setCoins(data);
+    const formattedResponse = formatMarketData(data);
+    setCoins(formattedResponse);
   };
   console.log(coins);
+
 
   useEffect(() => {
     fetchCoinsList();
   }, [currency]);
-
-  
 
   const formatNumber = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
 
   const options = {
-    legend: {
-      display: false
+    elements: {
+      point: {
+        radius: 1,
+      },
+    },
+    scales: {
+      x: [{
+        display: false
+      }]
     }
   };
 
-
-
   return (
     <ThemeProvider theme={darkTheme}>
-    <TableContainer component={Paper}>
-      <Grid item xs={12}>
-      <Table size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Ranking</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Current Price</TableCell>
-            <TableCell align="right">Percent Change</TableCell>
-            <TableCell align="right">Market Cap</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {coins.map((row) => {
-             const profit = row.price_change_percentage_24h > 0
-             return (
-            <TableRow
-              key={row.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.market_cap_rank}
-              </TableCell>
-              <TableCell component="th" scope="row">
-              <img src={row.image} style={{ height: 40 }} />{row.name}
-              </TableCell>
-              <TableCell align="right">{symbol}{row.current_price}</TableCell>
-              <TableCell align="right"   style={{
-                            color: profit > 0 ? "rgb(14, 203, 129)" : "red",
-                            fontWeight: 500,
-                          }}>
-                {profit && "+"}
-                {formatNumber(row?.price_change_percentage_24h.toFixed(2))}%
-              </TableCell>
-              {/* {/* <TableCell align="right">{row.carbs}</TableCell> */}
-              <TableCell align="right">{symbol}{formatNumber(row.market_cap)}</TableCell>
-              <TableCell>
-              <Line
-  data={{
-    labels: ['Jun', 'Jul', 'Aug'],
-    datasets: [
-      {
-        id: 1,
-        label: '',
-        data: [5, 6, 7],
-        borderColor: "#EEBC1D",
-      },
-      {
-        id: 2,
-        label: '',
-        data: [3, 2, 1],
-        borderColor: "#EEBC1D",
-      },
-    ],
-  
-  }}
-  options={{
-    plugins: {
-      legend: {
-        display: false
-      }
-    }
-  }}
-/>
-              </TableCell>
-            </TableRow>
-             )
-          
-          })}
-        </TableBody>
-      </Table>
-      </Grid>
-    </TableContainer>
+      <Container>
+        <Grid item xs={12}>
+          <Table aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Ranking</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Current Price</TableCell>
+                <TableCell align="right">Percent Change</TableCell>
+                <TableCell align="right">Market Cap</TableCell>
+                <TableCell>Last 24 Hours</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {coins.map((row) => {
+                const profit = row.price_change_percentage_24h > 0;
+                return (
+                  <TableRow
+                    key={row.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.market_cap_rank}
+                    </TableCell>
+                    <TableCell
+                      component={Link}
+                      to={`/coins/${row.id}`}
+                      scope="row"
+                    >
+                      <img src={row.image} style={{ height: 40 }} />
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">
+                      {symbol}
+                      {row.current_price}
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+                        fontWeight: 500,
+                      }}
+                    >
+                      {profit && "+"}
+                      {formatNumber(
+                        row?.price_change_percentage_24h.toFixed(2)
+                      )}
+                      %
+                    </TableCell>
+                    <TableCell align="right">
+                      {symbol}
+                      {formatNumber(row.market_cap)}
+                    </TableCell>
+                    <TableCell>
+                      <Line  data={{
+                          labels: row.sparkline_in_7d.price.map((data) => {
+                            let date = new Date(data.x*1000)
+                            let formattedDate = moment(date).format("MM/DD/YYYY HH:mm");
+                            return formattedDate;
+                          }
+                          ),
+
+                          datasets: [
+                            {
+                              data: row.sparkline_in_7d.price.map((data) => data.y),
+                              label: 'Price',
+                              borderColor: "#EEBC1D",
+                            }
+                          ]
+                      }}
+                      options={options}
+                      />
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Grid>
+      </Container>
     </ThemeProvider>
   );
 };
